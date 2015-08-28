5ddd78702c2a7a58db96ff3ef22879bf91a21284
Thiago Mori <thiago.mori@gmail.com>
2015-08-28 18:43:44 -0300

    - Instalando dependencias do frontend e iniciando layout;

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,10 +1,56 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <title>Agenda Telefonica</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Agenda Telefônica</title>
+
+    <!-- Bootstrap -->
+    <link href="css/app.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-    <h1>Agenda Telefonica</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 page-header">
+            <h1>
+                Code.Education<br/>
+                <small><i class="glyphicon glyphicon-phone-alt"></i> Agenda Telefônica</small>
+            <span class="pull-right">
+                <form action="#" method="POST" class="form-inline">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Pesquisar contato...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
+                        </span>
+                    </div>
+                </form>
+            </span>
+            </h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            @foreach(range('A' ,'Z') as $letra)
+                <a href="#" class="btn btn-xs btn-primary">{{ $letra }}</a>
+            @endforeach
+        </div>
+    </div>
+    <div class="row">
+        @yield('content')
+    </div>
+</div>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/scripts.js"></script>
 </body>
 </html>
